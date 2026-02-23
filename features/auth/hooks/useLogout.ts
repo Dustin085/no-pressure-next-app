9f0fbaa00184e16a2f586a644e188a667a9131aa
Dustin085 <dustin830927@gmail.com>
2026-02-23 23:33:48 +0800

    edit: force refresh when logout

--- features/auth/hooks/useLogout.ts
+++ features/auth/hooks/useLogout.ts
@@ -1,16 +1,17 @@
 'use client';
 import { supabase } from '@/lib/supabase/client';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 
 export function useLogout() {
-    const router = useRouter();
+    // const router = useRouter();
 
     const logout = async () => {
         
         const { error } = await supabase.auth.signOut();
-        
-        if (!error) router.push('/login');
+
+        // if (!error) router.push('/login');
+        if (!error) window.location.href = '/login';
         else console.error(error.message);
     };
 
